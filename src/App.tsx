a310b80d47aeddd0d67d8514ca2b72e0e82cc98f
alanbruno1994 <bruno47rios@outlook.com>
2021-12-01 19:18:58 -0300

    feat: create a small project using React.memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useCallback, useState } from 'react';
+
 import './App.css';
+import ComponentA from './components/ComponentA';
+
+import ComponentB from './components/ComponentB';
+import ComponentC from './components/ComponentC';
 
 function App() {
+  const [value,setValue]=useState(false)
+  const [name,setName]=useState('')
+  //Para componentA e componentB deve-se usar o Reac.memo combinado do useCallback por que nesses
+  //casos vai se passar uma funcao
+  const changeValue=useCallback((v:boolean)=>{ setValue(v) },[])
+
+  console.log('Run App',value)
+
+
+  //Para o componenteA que tem um props que so tem um tipo primitivo so o Rect.memo bastaria para evitar a execucao desnecessaria
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={name} onChange={event=>setName(event.target.value)}></input>
+      <ComponentA value={value}/>
+      <ComponentB setValue={changeValue}/>
+      <ComponentC setValue={changeValue}/>
     </div>
   );
 }
